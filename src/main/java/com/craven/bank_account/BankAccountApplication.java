3858e0063e4392657b203392f3dfa50653dc2087
cravencooper <cravencooper.cc@gmail.com>
2024-08-19 12:04:19 +0100

    BANK-ACCOUNT: Adding in new way to produce transactions as well as getting rid of DB backed storage.

--- src/main/java/com/craven/bank_account/BankAccountApplication.java
+++ src/main/java/com/craven/bank_account/BankAccountApplication.java
@@ -1,19 +1,14 @@
 package com.craven.bank_account;
 
-import com.craven.bank_account.transaction.CreditProducer;
-import com.craven.bank_account.transaction.DebitProducer;
+import com.craven.bank_account.transaction.TransactionsProducers;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
-@EntityScan("com.craven.bank_account.transaction.model")
-@EnableJpaRepositories("com.craven.bank_account.persistence")
 public class BankAccountApplication {
 
 	public static void main(String[] args) {
@@ -22,10 +17,10 @@ public class BankAccountApplication {
 	}
 
 	@Bean
-	public CommandLineRunner startProducers(CreditProducer creditProducer, DebitProducer debitProducer) {
+	public CommandLineRunner startProducers(TransactionsProducers transactionsProducers) {
 		return args -> {
-			creditProducer.produceCredits();
-			debitProducer.produceDebits();
+			transactionsProducers.generateCredits();
+			transactionsProducers.generateDebits();
 		};
 	}
 
