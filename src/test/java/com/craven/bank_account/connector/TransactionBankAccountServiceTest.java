2346b88f83491475c3104c90ba0d730750e90b00
cravencooper <cravencooper.cc@gmail.com>
2024-08-19 15:38:23 +0100

    BANK-ACCOUNT: Tidy up and testing.

--- src/test/java/com/craven/bank_account/connector/TransactionBankAccountServiceTest.java
+++ src/test/java/com/craven/bank_account/connector/TransactionBankAccountServiceTest.java
@@ -1,10 +1,18 @@
 package com.craven.bank_account.connector;
 
+import com.craven.bank_account.audit.AuditService;
+import com.craven.bank_account.transaction.model.Transaction;
+import com.craven.bank_account.transaction.persistence.TransactionPersistenceService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.UUID;
+
+import static com.craven.bank_account.transaction.model.Transaction.TransactionType.DEBIT;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
 import static org.mockito.MockitoAnnotations.openMocks;
 
 class TransactionBankAccountServiceTest {
@@ -12,13 +20,27 @@ class TransactionBankAccountServiceTest {
     @Autowired
     private TransactionBankAccountService underTest;
 
+
+    @Mock
+    private TransactionPersistenceService transactionPersistenceService;
+    @Mock
+    private AuditService auditService;
+    @Mock
+    private AuditServiceConfig auditServiceConfig;
+
     @BeforeEach
     void setUp() {
         openMocks(this);
+
+        underTest = new TransactionBankAccountService(transactionPersistenceService, auditService, auditServiceConfig);
     }
 
     @Test
     void willStoreProvidedTransaction() {
+        Transaction transaction = new Transaction(UUID.randomUUID(), DEBIT, 123.34);
 
+        underTest.processTransaction(transaction);
+
+        verify(transactionPersistenceService).storeTransaction(transaction);
     }
 }
